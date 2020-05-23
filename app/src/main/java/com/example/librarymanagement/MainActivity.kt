2574031741164c59f61767c20d00249f7a3bf9e4
Caplimbo <zhhqu0305@gmail.com>
2020-05-23 16:25:09 +0800

    jump_realized

--- app/src/main/java/com/example/librarymanagement/MainActivity.kt
+++ app/src/main/java/com/example/librarymanagement/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.librarymanagement.ui.activity.RoomTest
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_room_test.*
 
 
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        test_bar.setOnClickListener {
+        index.setOnClickListener {
             //val intent = Intent(this, RoomTest::class.java)
             //startActivity(intent)
             Intent(this, RoomTest::class.java).apply { startActivity(this) }
--- app/src/main/java/com/example/librarymanagement/ui/activity/RoomTest.kt
+++ app/src/main/java/com/example/librarymanagement/ui/activity/RoomTest.kt
@@ -1,13 +1,21 @@
 package com.example.librarymanagement.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.librarymanagement.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_room_test.*
 
 class RoomTest : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_test)
+        test_bar.setOnClickListener {
+            //val intent = Intent(this, RoomTest::class.java)
+            //startActivity(intent)
+            Intent(this, RoomTest::class.java).apply { startActivity(this) }
+        }
     }
 }
